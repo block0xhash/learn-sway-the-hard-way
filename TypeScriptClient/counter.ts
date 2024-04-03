e76e50a53342f4b55911100b0a73734916370e78
block0xhash <crypt0mate@proton.me>
2024-04-03 22:01:44 +0200

    fixed counter.ts and deploy.ts

--- TypeScriptClient/counter.ts
+++ TypeScriptClient/counter.ts
@@ -1,5 +1,5 @@
 import { Provider, Wallet, Contract, BigNumberish, BN } from "fuels";
-import abi from "../counter/out/debug/counter-abi.json";
+import abi from "../Counter/out/debug/counter-abi.json";
 
 async function main() {
 
@@ -10,9 +10,7 @@ async function main() {
     // console.log(wallet);
 
     const contractId = "0x53f14c86e9dc7f025aa54ba75a9d9da609b86e6c8cee56f55e17dd04763a9997";
-   
-    const x = 'fuel1ekhkxpvzjlg5zed7rwadhdse94a66x5jfxykv36d75cj4fzzq9nq42t6gl';
-    const counter = new Contract(x, abi, wallet);
+    const counter = new Contract(contractId, abi, wallet);
 
     const { transactionId, value } = await counter.functions.inc_counter().call();
     console.log(transactionId);
--- TypeScriptClient/deploy.ts
+++ TypeScriptClient/deploy.ts
@@ -1,6 +1,6 @@
 import { Provider, ContractFactory, Wallet } from "fuels";
 import { readFileSync } from 'fs';
-import abi from "../counter/out/debug/counter-abi.json";
+import abi from "../Counter/out/debug/counter-abi.json";
 
 async function main() {
 
@@ -8,7 +8,7 @@ async function main() {
     const PRIVATE_KEY = process.env.PRIVATE_KEY!;
     const wallet = Wallet.fromPrivateKey(PRIVATE_KEY, provider);
 
-    const bytecode = readFileSync("../counter/out/debug/counter.bin");
+    const bytecode = readFileSync("../Counter/out/debug/counter.bin");
 
     const factory = new ContractFactory(bytecode, abi, wallet);
     const contract = await factory.deployContract();
